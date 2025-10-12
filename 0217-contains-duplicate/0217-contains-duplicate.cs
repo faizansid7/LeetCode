263eed991749a0118f9eed165dd0668a18ae4f0e
muhammad faizan siddiqui <43236167+faizansid7@users.noreply.github.com>
2025-10-12 21:58:14 +0500

    Time: 16 ms (69.14%), Space: 67.7 MB (60.09%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        HashSet<int> hash = new HashSet<int>();
+		for (int i = 0; i < nums.Length; i++)
+		{
+			if(!hash.Add(nums[i])){
+				return true;
+			}
+		}
+		return false;
+    }
+}
