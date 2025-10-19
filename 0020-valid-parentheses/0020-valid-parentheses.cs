229a25995989efd856ac11be85ec8de904baa938
muhammad faizan siddiqui <43236167+faizansid7@users.noreply.github.com>
2025-10-19 20:38:12 +0500

    Time: 21 ms (5.32%), Space: 42.5 MB (22.99%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public bool IsValid(string str) {
+        Stack<char> stack = new Stack<char>();
+        if(str.Length < 2){
+			return false;
+		}
+        for(int i=0; i<str.Length; i++)
+		{
+			if(str[i]=='(' || str[i]=='{' || str[i]=='[')
+			{
+				stack.Push(str[i]);
+			}
+			else
+			{
+                if(stack.Count == 0)
+					return false; 
+				var bracket = stack.Pop();
+				Console.WriteLine(bracket);
+				if((bracket == '(' && str[i] != ')') || 
+                (bracket == '{' && str[i] != '}') || 
+                (bracket == '[' && str[i] != ']'))
+				{
+					return false;
+				}
+			}	
+		}
+        if(stack.Count > 0)
+			return false; 
+        return true;
+    }
+}
