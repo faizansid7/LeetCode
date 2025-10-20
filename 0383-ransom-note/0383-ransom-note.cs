54055de910b51821e9be6ccadbef309fe561b96c
muhammad faizan siddiqui <43236167+faizansid7@users.noreply.github.com>
2025-10-20 21:16:27 +0500

    Time: 9 ms (49.9%), Space: 46.1 MB (79.87%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public bool CanConstruct(string ransomNote, string magazine) {
+        if(magazine.Length < ransomNote.Length)
+            return false;
+        
+        Dictionary<char, int> pool = new Dictionary<char, int>();
+        for(int i=0; i<magazine.Length; i++){
+            if(!pool.ContainsKey(magazine[i])){
+                pool.Add(magazine[i],1);
+			}
+            else{
+				pool.TryGetValue(magazine[i], out int val);
+				pool[magazine[i]] = val + 1;
+			}
+        }
+		for(int i=0; i<ransomNote.Length; i++){
+			var charPresent = pool.TryGetValue(ransomNote[i], out int freq);
+			if(!charPresent || freq == 0)
+				return false;
+			pool[ransomNote[i]] = freq-1;
+		}
+		return true;
+    }
+}
